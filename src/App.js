2900989f5730f6fbf1fad920f909c94d26abe3ae
lcoo1 <varon12@naver.com>
2023-03-05 18:39:33 +0900

    layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import Menubar from "./layout/Menubar"
+import Home from './Home'
+import Footer from "./layout/Footer"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          유튜브 쇼츠 시작
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+        <div>
+            <Menubar/>
+
+            <Home/>
+
+            <Footer/>
+        </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/layout/Footer.js
@@ -0,0 +1,13 @@
+import "./layout.css"
+const ButtonAppBar = () => {
+    return (
+        <footer>
+            <hr/>
+            <div class="font">
+                푸터푸터
+            </div>
+        </footer>
+    )
+}
+
+export default ButtonAppBar;
