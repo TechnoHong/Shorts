239281805a29376bbe4366ed8024f403403f4843
Seokjun Hong <sjhong@ocube.co.kr>
2023-03-07 13:45:17 +0900

    [refactoring] ESLint, Prettier 적용

---
+++ src/layout/Footer.jsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import './layout.css';
+
+const ButtonAppBar = () => {
+  return (
+    <footer>
+      <hr />
+      <div className="font">푸터푸터</div>
+    </footer>
+  );
+};
+
+export default ButtonAppBar;
