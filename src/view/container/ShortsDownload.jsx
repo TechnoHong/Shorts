362657e62ee479bb61ca112f0fc5303a6b85b1ae
Seokjun Hong <sjhong@ocube.co.kr>
2023-03-08 17:20:56 +0900

    [feat] 찌꺼기

--- src/view/container/ShortsDownload.jsx
+++ src/view/container/ShortsDownload.jsx
@@ -1,7 +1,23 @@
 import React from 'react';
+import { Container, Paper, Stack, styled } from '@mui/material';
 
 const ShortsDownload = () => {
-  return <div>download page</div>;
+  return (
+    <Container maxWidth="sm">
+      <Stack spacing={2}>
+        <Item>item1</Item>
+        <Item>item2</Item>
+        <Item>item3</Item>
+      </Stack>
+    </Container>
+  );
 };
+
+const Item = styled(Paper)(({ theme }) => ({
+  ...theme.typography.body2,
+  padding: theme.spacing(1),
+  textAlign: 'center',
+  color: theme.palette.text.secondary,
+}));
 
 export default ShortsDownload;
