e306559fda9d595703e859a19ace15b77e5f5372
Seokjun Hong <sjhong@ocube.co.kr>
2023-03-14 13:58:43 +0900

    [design] Theming #1

--- src/theme/GlobalTheme.jsx
+++ src/theme/GlobalTheme.jsx
@@ -1,15 +1,67 @@
 import { createTheme } from '@mui/material';
-import { blue, green } from '@mui/material/colors';
+import { blue, green, red } from '@mui/material/colors';
 
 export const theme = (mode) =>
   createTheme({
+    components: {
+      MuiCssBaseline: {
+        styleOverrides: {
+          html: {
+            height: '100%',
+          },
+          body: {
+            margin: 0,
+            padding: 0,
+            height: '100%',
+          },
+          '#root': {
+            height: '100%',
+            display: 'flex',
+            flexDirection: 'column',
+          },
+        },
+      },
+    },
     palette: {
       mode,
-      primary: {
-        main: blue[500],
-      },
-      secondary: {
-        main: green[500],
-      },
+      ...(mode === 'light'
+        ? {
+            primary: {
+              main: blue[500],
+            },
+            secondary: {
+              main: green[500],
+            },
+            text: {
+              primary: blue[500],
+            },
+          }
+        : {
+            primary: {
+              main: blue[500],
+            },
+            secondary: {
+              main: green[500],
+            },
+            text: {
+              primary: red[500],
+            },
+          }),
+    },
+    typography: {
+      fontFamily: [
+        'Noto Sans KR',
+        'sans-serif',
+        '-apple-system',
+        'BlinkMacSystemFont',
+        'Segoe UI',
+        'Roboto',
+        'Oxygen',
+        'Ubuntu',
+        'Cantarell',
+        'Fira Sans',
+        'Droid Sans',
+        'Helvetica Neue',
+      ].join(','),
     },
   });
