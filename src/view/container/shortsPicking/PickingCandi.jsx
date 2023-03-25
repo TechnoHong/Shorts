e3a2afe086df5278d12712769e11f3200bdfa0ed
yun <minefor47@gmail.com>
2023-03-25 19:36:54 +0900

    3.25 list 수정

--- src/view/container/shortsPicking/PickingCandi.jsx
+++ src/view/container/shortsPicking/PickingCandi.jsx
@@ -1,48 +1,326 @@
 import * as React from 'react';
-import List from '@mui/material/List';
-import ListItem from '@mui/material/ListItem';
 import IconButton from '@mui/material/IconButton';
-import { Tooltip } from '@mui/material';
 import {
-  ArrowRight,
-  Download,
-  PlayCircle,
-  Settings,
-} from '@mui/icons-material';
+  alpha,
+  Box,
+  Checkbox,
+  Paper, Table, TableBody,
+  TableCell,
+  TableContainer,
+  TableHead, TablePagination,
+  TableRow, TableSortLabel,
+  Toolbar,
+  Tooltip
+} from '@mui/material';
+import Typography from "@mui/material/Typography";
+import PropTypes from "prop-types";
+import FilterListIcon from '@mui/icons-material/FilterList';
+import { visuallyHidden } from '@mui/utils';
+import {Download} from "@mui/icons-material";
+
+
+// num, time, rate / download
+function createData(rank, rate, timet) {
+  return {
+    rank,
+    rate,
+    timet,
+  };
+}
+
+const rows = [
+  createData(1,30,'01:20:40 - 01:20:50', ),
+  createData(2,19,'01:00:10 - 01:00:20', ),
+  createData(3,17,'00:20:30 - 00:20:40', ),
+  createData(4,12,'00:53:50 - 00:54:00', ),
+  createData(5,23,'01:12:20 - 01:12:30', ),
+];
+function descendingComparator(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1;
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1;
+  }
+  return 0;
+}
+
+function getComparator(order, orderBy) {
+  return order === 'desc'
+      ? (a, b) => descendingComparator(a, b, orderBy)
+      : (a, b) => -descendingComparator(a, b, orderBy);
+}
+
+// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
+// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
+// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
+// with exampleArray.slice().sort(exampleComparator)
+function stableSort(array, comparator) {
+  const stabilizedThis = array.map((el, index) => [el, index]);
+  stabilizedThis.sort((a, b) => {
+    const order = comparator(a[0], b[0]);
+    if (order !== 0) {
+      return order;
+    }
+    return a[1] - b[1];
+  });
+  return stabilizedThis.map((el) => el[0]);
+}
+
+const headCells = [
+  {
+    id: 'rank',
+    numeric : true,
+    label: 'rank',
+  },
+  {
+    id: 'rate',
+    numeric : true,
+    label: 'rate (%)',
+  },
+  {
+    id: 'timet',
+    label: 'timestamp',
+  },
+];
+
+function EnhancedTableHead(props) {
+  const { onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort } =
+      props;
+  const createSortHandler = (property) => (event) => {
+    onRequestSort(event, property);
+  };
+
+  return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+                color="primary"
+                indeterminate={numSelected > 0 && numSelected < rowCount}
+                checked={rowCount > 0 && numSelected === rowCount}
+                onChange={onSelectAllClick}
+                inputProps={{
+                  'aria-label': 'select all desserts',
+                }}
+            />
+          </TableCell>
+          {headCells.map((headCell) => (
+              <TableCell
+                  key={headCell.id}
+                  align={'center'}
+                  padding={'normal'}
+                  sortDirection={orderBy === headCell.id ? order : false}
+              >
+                <TableSortLabel
+                    active={orderBy === headCell.id}
+                    direction={orderBy === headCell.id ? order : 'asc'}
+                    onClick={createSortHandler(headCell.id)}
+                >
+                  {headCell.label}
+                  {orderBy === headCell.id ? (
+                      <Box component="span" sx={visuallyHidden}>
+                        {order === 'desc' ? 'sorted descending' : 'sorted ascending'}
+                      </Box>
+                  ) : null}
+                </TableSortLabel>
+              </TableCell>
+          ))}
+        </TableRow>
+      </TableHead>
+  );
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.oneOf(['asc', 'desc']).isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+};
+
+function EnhancedTableToolbar(props) {
+  const { numSelected } = props;
+
+  return (
+      <Toolbar
+          sx={{
+            pl: { sm: 2 },
+            pr: { xs: 1, sm: 1 },
+            ...(numSelected > 0 && {
+              bgcolor: (theme) =>
+                  alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
+            }),
+          }}
+      >
+        {numSelected > 0 ? (
+            <Typography
+                sx={{ flex: '1 1 100%' }}
+                color="inherit"
+                variant="subtitle1"
+                component="div"
+            >
+              {numSelected} selected
+            </Typography>
+        ) : (
+            <Typography
+                sx={{ flex: '1 1 100%' }}
+                variant="h6"
+                id="tableTitle"
+                component="div"
+            >
+              Shorts
+            </Typography>
+        )}
+
+        {numSelected > 0 ? (
+            <Tooltip title="Delete">
+              <IconButton>
+                <Download />
+              </IconButton>
+            </Tooltip>
+        ) : (
+            <Tooltip title="Filter list">
+              <IconButton>
+                <FilterListIcon />
+              </IconButton>
+            </Tooltip>
+        )}
+      </Toolbar>
+  );
+}
+
+EnhancedTableToolbar.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+};
 
 const PickingCandi = () => {
+  const [order, setOrder] = React.useState('asc');
+  const [orderBy, setOrderBy] = React.useState('rate');
+  const [selected, setSelected] = React.useState([]);
+  const [page, setPage] = React.useState(0);
+  const [rowsPerPage, setRowsPerPage] = React.useState(5);
+
+  const handleRequestSort = (event, property) => {
+    const isAsc = orderBy === property && order === 'asc';
+    setOrder(isAsc ? 'desc' : 'asc');
+    setOrderBy(property);
+  };
+
+  const handleSelectAllClick = (event) => {
+    if (event.target.checked) {
+      const newSelected = rows.map((n) => n.rank);
+      setSelected(newSelected);
+      return;
+    }
+    setSelected([]);
+  };
+
+  const handleClick = (event, name) => {
+    const selectedIndex = selected.indexOf(name);
+    let newSelected = [];
+
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, name);
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1));
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1));
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+          selected.slice(0, selectedIndex),
+          selected.slice(selectedIndex + 1),
+      );
+    }
+
+    setSelected(newSelected);
+  };
+
+  const handleChangePage = (event, newPage) => {
+    setPage(newPage);
+  };
+
+  const handleChangeRowsPerPage = (event) => {
+    setRowsPerPage(parseInt(event.target.value, 10));
+    setPage(0);
+  };
+
+  const isSelected = (name) => selected.indexOf(name) !== -1;
+
   return (
-    <List sx={{ width: '100%', maxWidth: 1000 }}>
-      {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9].map((value) => {
-        return (
-          <List key={value} component="nav" disablePadding>
-            <ListItem component="div" disablePadding>
-              <Tooltip
-                title={`${value + 1}등 > Shorts ${
-                  value + 1
-                } 00:00:00 - 00:00:00`}
-              >
-                <IconButton
-                  size="large"
-                  sx={{
-                    display: 'flex',
-                    alignItems: 'baseline',
-                    alignSelf: 'start',
-                    gap: '0.5rem',
-                  }}
-                >
-                  <Settings />
-                  <ArrowRight />
-                  <Download />
-                  <PlayCircle />
-                </IconButton>
-              </Tooltip>
-            </ListItem>
-          </List>
-        );
-      })}
-    </List>
+      <Box sx={{ width: '100%' }}>
+        <Paper sx={{ width: '100%', mb: 2 }}>
+          <EnhancedTableToolbar numSelected={selected.length} />
+          <TableContainer>
+            <Table
+                aria-labelledby="tableTitle"
+            >
+              <EnhancedTableHead
+                  numSelected={selected.length}
+                  order={order}
+                  orderBy={orderBy}
+                  onSelectAllClick={handleSelectAllClick}
+                  onRequestSort={handleRequestSort}
+                  rowCount={rows.length}
+              />
+              <TableBody>
+                {stableSort(rows, getComparator(order, orderBy))
+                    .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                    .map((row, index) => {
+                      const isItemSelected = isSelected(row.name);
+                      const labelId = `enhanced-table-checkbox-${index}`;
+
+                      return (
+                          <TableRow
+                              hover
+                              onClick={(event) => handleClick(event, row.name)}
+                              role="checkbox"
+                              aria-checked={isItemSelected}
+                              tabIndex={-1}
+                              key={row.name}
+                              selected={isItemSelected}
+                              sx={{ cursor: 'pointer' }}
+                          >
+                            <TableCell padding="checkbox" >
+                              <Checkbox
+                                  color="primary"
+                                  checked={isItemSelected}
+                                  inputProps={{
+                                    'aria-labelledby': labelId,
+                                  }}
+                              />
+                            </TableCell>
+                            <TableCell
+                                component="th"
+                                id={labelId}
+                                scope="row"
+                                padding="none"
+                                align="center"
+                            >
+                              {row.rank}
+                            </TableCell>
+                            <TableCell align="center">{row.rate}</TableCell>
+                            <TableCell align="center">{row.timet}</TableCell>
+                          </TableRow>
+                      );
+                    })}
+
+              </TableBody>
+            </Table>
+          </TableContainer>
+          <TablePagination
+              rowsPerPageOptions={[5, 10, 25]}
+              component="div"
+              count={rows.length}
+              rowsPerPage={rowsPerPage}
+              page={page}
+              onPageChange={handleChangePage}
+              onRowsPerPageChange={handleChangeRowsPerPage}
+          />
+        </Paper>
+      </Box>
   );
-};
+}
 // <ListItemText id={labelId} primary={`${value + 1}등 > Shorts ${value + 1} 00:00:00 - 00:00:00`} />
 export default PickingCandi;
