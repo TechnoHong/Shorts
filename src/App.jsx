033d58a7101f0fb48deac83cad33bf7fc3f5c155
Seokjun Hong <sjhong@ocube.co.kr>
2023-03-30 11:20:55 +0900

    [remove] Download 관련 컴포넌트 삭제 / 샘플 컴포넌트 삭제

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,6 @@ import Footer from './view/Footer';
 import { ThemeProvider } from '@mui/material';
 import { ColorModeContext } from './context/ColorContext';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import ShortsDownload from './view/container/shortDownload/ShortsDownload';
 import NotFound from './view/container/NotFound';
 import CssBaseline from '@mui/material/CssBaseline';
 import { theme } from './theme/GlobalTheme';
@@ -34,7 +33,6 @@ const App = () => {
 
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/download" element={<ShortsDownload />} />
             <Route path="/ShortsPicking" element={<ShortsPicking />} />
             <Route path="*" element={<NotFound />} />
           </Routes>
--- src/api/CommonAxios.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-export const instance = axios.create({
-  baseURL: 'http://15.165.149.51',
-  timeout: 1000,
-});
